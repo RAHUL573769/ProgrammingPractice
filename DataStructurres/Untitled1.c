97366febb4c84151d108310e64e05ee2bc5f90e6
Rahul Rudra <rahulrudra665@gmail.com>
2023-03-04 19:32:06 +0600

    array commit

--- DataStructurres/Untitled1.c
+++ DataStructurres/Untitled1.c
@@ -4,10 +4,10 @@
 int main(){
 
 
-int array[100],n,i;
+int array[100],n,i,loc;
 printf("Enter now many elements\n");
 scanf("%d",&n);
-
+printf("Enter %d elements\n",n);
 for(i=0;i<n;i++){
 
     scanf("%d",&array[i]);
@@ -18,9 +18,38 @@ for(i=0;i<n;i++){
     printf("%d\n",array[i]);
 }
 
+printf("\n Enter the location you want to delete\n");
 
 
 
+scanf("%d",&loc);
+
+
+if(loc>n){
+    printf("Deletion not possible");
+}
+
+else{
+
+        i=0;
+while(i!=loc-1){
+    i++;
+
+}
+
+while(i<n){
+    array[i]=array[i+1];
+    i++;
+}
+n--;
+
+printf("The new Array\n");
+for(i=0;i<n;i++){
+
+    printf("%d\n",array[i]);
+}
+
+}
 return 0;
 }
 
