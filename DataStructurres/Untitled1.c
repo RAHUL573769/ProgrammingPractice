73a7d55bb7456036cef242367134c25d92ac93a4
Rahul Rudra <rahulrudra665@gmail.com>
2023-03-04 10:23:55 +0600

    new commit array:

--- DataStructurres/Untitled1.c
+++ DataStructurres/Untitled1.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+
+int array[100],n,i;
+printf("Enter now many elements\n");
+scanf("%d",&n);
+
+for(i=0;i<n;i++){
+
+    scanf("%d",&array[i]);
+}
+printf("The elements of array are\n");
+for(i=0;i<n;i++){
+
+    printf("%d\n",array[i]);
+}
+
+
+
+
+return 0;
+}
+
+
+
 
