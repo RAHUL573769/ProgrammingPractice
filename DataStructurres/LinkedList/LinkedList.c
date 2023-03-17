cada1fedeef13f0cbdf192b1873962d561df300f
Rahul Rudra <rahulrudra665@gmail.com>
2023-03-17 14:33:49 +0600

    linked lisr new added

--- DataStructurres/LinkedList/LinkedList.c
+++ DataStructurres/LinkedList/LinkedList.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+
 struct Node{
 	int data;
 	struct Node *next;
 };
+
+
+void linkedListTraversal(struct Node *ptr){
+	
+	while(ptr!=NULL){
+			printf("%d",ptr->data);
+			ptr=ptr->next;
+	}
+
+	
+}
 
 int main(){
 	struct Node *head;
@@ -19,6 +32,6 @@ int main(){
 		second->next=third;
 		third->data=8;
 		third->next=NULL;
-	
+	linkedListTraversal(head);
 	return 0;
 }
