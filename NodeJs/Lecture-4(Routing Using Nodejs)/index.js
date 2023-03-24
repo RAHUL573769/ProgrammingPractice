8b81613352af1e2ba4838b8115430377d7a0b0f3
Rahul Rudra <rahulrudra665@gmail.com>
2023-03-24 10:08:39 +0600

    package commit

---
+++ NodeJs/Lecture-4(Routing Using Nodejs)/index.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const fs = require('fs');
+const server=http.createServer((req,res)=>{
+
+
+    const handleReadFile=(statucode,fileLocation)=>{
+        fs.readFile(fileLocation,(err,data)=>{
+            res.writeHead(statucode,{'Content-Type':"text/html"});
+            res.write(data);
+            res.end();
+          })
+
+    }
+
+
+    if(req.url==='/'){
+        handleReadFile(200,'./views/index.html');
+        // fs.readFile('./views/index.html',(err,data)=>{
+        //   res.writeHead(200,{'Content-Type':"text/html"});
+        //   res.write(data);
+        //   res.end();
+        // })
+    }
+
+    else if(req.url=='./views/about'){
+        fs.readFile('about.html',(err,data)=>{
+            res.writeHead(200,{'Content-Type':"text/html"});
+            res.write(data);
+            res.end();
+          })
+
+    }
+    else{
+
+        fs.readFile('contact.html',(err,data)=>{
+            res.writeHead(200,{'Content-Type':"text/html"});
+            res.write(data);
+            res.end();
+          })
+
+
+    }
+
+
+
+});
+
+
+
+
+server.listen(3000,(req,res)=>{
+    console.log("server running");
+})
