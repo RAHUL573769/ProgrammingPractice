c0f61ab8b4a8f8a1a8e04f095bc072f3d2b35999
Rahul Rudra <rahulrudra665@gmail.com>
2023-03-27 10:46:13 +0600

    variables added

---
+++ C++ Programs/Week1/opratorsnew2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	
+	int x;
+	float y;
+	string a;
+	double b;
+	
+	cout<<"Enter the value x "<<"\n";
+	cin>>x;
+		cout<<"Enter the value y"<<"\n";
+	cin>>y;
+		cout<<"Enter the value a"<<"\n";
+	cin>>a;
+		cout<<"Enter the value b"<<"\n";
+	cin>>b;
+	cout<<"The value of xis "<<x;
+		cout<<"The value of y is "<<y;
+			cout<<"The value of a is "<<a;
+				cout<<"The value of b is "<<b;
+}
+
