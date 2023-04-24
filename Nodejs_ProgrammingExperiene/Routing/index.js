73bf78365c477af582e06093c4daf2562dda40bc
Rahul Rudra <rahulrudra665@gmail.com>
2023-04-24 15:25:14 +0600

    middle ware commit

--- Nodejs_ProgrammingExperiene/Routing/index.js
+++ Nodejs_ProgrammingExperiene/Routing/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const userRouter = require("./router/userRouter");
 const { getDynamicData } = require("./controllers/userController");
-console.log(userRouter);
+
 app.use("/", userRouter);
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/dynamic", userRouter);
+app.use("/middle", (req, res, next) => {
+  res.send("Hdello");
+  next();
+});
+
+app.get("*", (req, res) => {
+  res.send("404 Not Found");
+});
 app.listen(3000, (req, res) => {
   console.log("Server s Running");
 });
