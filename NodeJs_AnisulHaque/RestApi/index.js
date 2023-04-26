6c8cb56cfe7cec3dbafeb87279fa825c05fc6595
Rahul Rudra <rahulrudra665@gmail.com>
2023-04-26 14:59:06 +0600

    changes commit

--- NodeJs_AnisulHaque/RestApi/index.js
+++ NodeJs_AnisulHaque/RestApi/index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = 3000;
+const cors = require("cors");
+const path = require("path");
+var bodyParser = require("body-parser");
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./", "views", "index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
