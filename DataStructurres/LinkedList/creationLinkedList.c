914940bdf42941ce2c23d195152f65d0b53d72dc
Rahul Rudra <rahulrudra665@gmail.com>
2023-05-31 10:43:38 +0600

    new linked commit

--- DataStructurres/LinkedList/creationLinkedList.c
+++ DataStructurres/LinkedList/creationLinkedList.c
@@ -11,13 +11,49 @@ struct node *first;
 struct node *middle;
 struct node *last;
 
-void traversal(struct node *p)
+struct node *head = NULL;
+
+void insertAtBeginning(int value)
 {
 
-    while (p != NULL)
+    struct node *newNode = malloc(sizeof(struct node));
+
+    newNode->data = value;
+    newNode->next = head;
+    head = newNode;
+}
+
+void insertAtLast(int value)
+{
+
+    struct node *head = NULL;
+
+    struct node *newNode = malloc(sizeof(struct node));
+
+    newNode->data = value;
+    newNode->next = NULL;
+    if (head == NULL)
     {
-        printf("The value is %d \n", p->data);
-        p = p->next;
+        head = newNode;
+    }
+    else
+    {
+        struct node *last = head;
+        while (last != NULL)
+        {
+
+            last = last->next;
+        }
+        last->next = newNode;
+    }
+}
+void traversal()
+{
+    struct node *temp = head;
+    while (temp != NULL)
+    {
+        printf("The value is %d \n", temp->data);
+        temp = temp->next;
     }
 }
 int main()
@@ -26,13 +62,17 @@ int main()
     first = malloc(sizeof(struct node));
     middle = malloc(sizeof(struct node));
     last = malloc(sizeof(struct node));
-    first->data = 16;
-    first->next = middle;
-    middle->data = 45;
-    middle->next = last;
-    last->data = 13;
-    last->next = NULL;
+    // first->data = 16;
+    // first->next = middle;
+    // middle->data = 45;
+    // middle->next = last;
+    // last->data = 13;
+    // last->next = NULL;
 
-    traversal(first);
+    traversal();
+    insertAtBeginning(12);
+    traversal();
+    insertAtLast(23);
+    traversal();
     return 0;
 }
