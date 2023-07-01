120e73ed9c0a4795b06ea3e6bb07d9413f7f4cb6
Rahul Rudra <rahulrudra665@gmail.com>
2023-07-01 14:46:20 +0600

    calvulator commit

--- Python/Module-5/classIntro.py
+++ Python/Module-5/classIntro.py
@@ -3,3 +3,30 @@ class Phone:
     name='samsung'
 myPhone=Phone()
 print(myPhone.price)
+
+class Person:
+    name='Rahul'
+    type='human'
+    
+    def call(self):
+        return 'call done'
+    def message(self,first,second):
+        text=f'{first} message {second} message'
+        return text
+m_phone=Person()
+print(m_phone.call())
+print(m_phone.message("Hello", "Python"))
+
+
+class calculator:
+    name='Ac Casino'
+    def sum(self,a,b):
+        return a+b
+    def substract(self,a,b):
+        return a-b
+    
+my_calculator=calculator()
+calcName=my_calculator.name
+print(calcName)
+sumOfCalculator=my_calculator.sum(1,3)
+print(sumOfCalculator)
