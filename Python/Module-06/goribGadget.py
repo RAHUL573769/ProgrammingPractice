40fd996750bc835c438eb59a61e3a16a178c3c17
Rahul Rudra <rahulrudra665@gmail.com>
2023-07-02 15:00:21 +0600

    inheritence main commit

--- Python/Module-06/goribGadget.py
+++ Python/Module-06/goribGadget.py
@@ -14,3 +14,21 @@ myLaptop=Laptop(price=1234,color='blue',memory=123,name="Asus")
 print(myLaptop)
 
 print(f'My Laptop Name is {myLaptop.name}')
+
+
+class Device:
+    def __init__(self,name,price,color) -> None:
+        self.name=name
+        self.price=price
+        self.color=color
+    def run(self):
+        return f'Running {self.name}'
+class Camera(Device):
+    def __init__(self,pixel,name,price,color) -> None:
+        self.pixel=pixel
+        super().__init__(name,price,color)
+
+myCamera=Camera(color='Blue',name='Oppo',pixel=123,price=12)
+myCamera.name='Rahul'
+print(myCamera.name)
+        
