1de2ad69771c702068f7f72cd3c31b55a816b687
Rahul Rudra <rahulrudra665@gmail.com>
2023-07-04 09:35:42 +0600

    sql commit

---
+++ Database Mangement/SqlFiles/Day1.sql
@@ -0,0 +1,11 @@
+create database studentinfo;
+use studentinfo;
+create table students(
+
+roll int,
+name varchar(20),
+contact varchar(40),
+email varchar(20)
+
+);
+insert into students(roll,name,contact,email) values (12,'Rahul','69,Souj','rahul@gmail.com')
