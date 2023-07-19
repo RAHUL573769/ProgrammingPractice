2369b9017517ebb29cf0c3b57fddce3712ccd3a2
Rahul Rudra <rahulrudra665@gmail.com>
2023-07-19 08:10:10 +0600

    daaabsee commit

--- Database Mangement/SqlFiles/practise_module52.sql
+++ Database Mangement/SqlFiles/practise_module52.sql
@@ -1,3 +1,36 @@
 create database employees;
 use employees;
+create table employees_data(
 
+id int auto_increment,
+f_name varchar(20) not null,
+m_name varchar(20) null,
+l_namr varchar(20) not null,
+age int not null,
+current_status varchar(20) not null default "employee name",
+constraint pKey primary key(id)
+);
+desc employees_data;
+
+
+CREATE TABLE cats (
+    cat_id INT AUTO_INCREMENT,
+    name VARCHAR(100),
+    breed VARCHAR(100),
+    age INT,
+    PRIMARY KEY (cat_id)
+); 
+
+
+INSERT INTO cats(name, breed, age) 
+VALUES ('Ringo', 'Tabby', 4),
+       ('Cindy', 'Maine Coon', 10),
+       ('Dumbledore', 'Maine Coon', 11),
+       ('Egg', 'Persian', 4),
+       ('Misty', 'Tabby', 13),
+       ('George Michael', 'Ragdoll', 9),
+       ('Jackson', 'Sphynx', 7);
+       desc cats;
+       
+       select * from cats;
+       
