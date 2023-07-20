e0ce1a5b88ef1a6b4e729cbc77c65a34c2d09489
Rahul Rudra <rahulrudra665@gmail.com>
2023-07-20 07:42:21 +0600

    crud challenge done

--- Database Mangement/SqlFiles/crudChallenge.sql
+++ Database Mangement/SqlFiles/crudChallenge.sql
@@ -16,4 +16,25 @@ insert into shirts(shirt_id,article,color,shirt_size,last_worn) values ('1','t-s
 ('5','t-shirt', 'pink', 'S', 0),
 ("6",'polo shirt', 'red', 'M', 5),
 ("7",'tank top', 'white', 'S', 200),
-( "8",'tank top', 'blue', 'M', 15)
+( "8",'tank top', 'blue', 'M', 15);
+
+desc shirts;
+select * from shirts;
+insert into shirts(shirt_id,article,color,shirt_size,last_worn) values ('9','polo shirt','purple','M',50);
+select * from shirts;
+select article,color from shirts;
+select * from shirts;
+select article,color,shirt_size,last_worn from shirts where shirt_size='M';
+select * from shirts;
+update shirts set shirt_size='L' where article="polo shirt";
+select shirt_size from shirts where article="polo shirt";
+
+
+update shirts set last_worn=0 where last_worn=15;
+
+select shirt_size ,color from shirts shirt_size ,color;
+update shirts set shirt_size='Xs ' ,color='off white'  where color='white';
+select shirt_size ,color from shirts where color='off white';
+
+select * from shirts;
+delete from shirts;
