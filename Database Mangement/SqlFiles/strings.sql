7974c1a5f98bed89de88d539d603c0297192dc73
Rahul Rudra <rahulrudra665@gmail.com>
2023-07-25 10:07:34 +0600

    mysql commit

--- Database Mangement/SqlFiles/strings.sql
+++ Database Mangement/SqlFiles/strings.sql
@@ -1,1 +1,8 @@
 use booksDb;
+select * from books;
+select title as Title from books limit 10,2;
+
+select released_year ,count(*) from books group by released_year;
+
+select title from books where released_year!=2020;
+select title ,pages as BookTitlePages from books where pages between 100 and 300 and title='The Namesake';
--- Python/Module-08(Udemy)/inhert.py
+++ Python/Module-08(Udemy)/inhert.py
@@ -40,8 +40,48 @@ class Dog(Animal):
         self.name=name
         Animal.__init__(self,type)
     def mySound(self):
-        return print(self.type +self.name+'Can Make Sound')
+        return print(self.type +self.name+' Can Make Sound ')
 
 myDog1=Animal(type='Dog')
 felix=Dog(name='felix',type='Dog')
 felix.mySound()
+
+
+class Vehicle:
+    def __init__(self,name) -> None:
+        self.name=name
+        pass    
+class Car(Vehicle):
+    def __init__(self, name,capacity,color) -> None:
+        self.color=color
+        self.capacity=capacity
+        Vehicle.__init__(self,name)
+    def myMethod(self):
+        return print(self.name+'is '+self.color+ 'is of capacity'+ self.capacity)
+class PrivateCar(Car):
+    def __init__(self, name, capacity, color,ac) -> None:
+        self.ac=ac
+        
+        Car.__init__(self,name, capacity, color)
+        
+
+
+corolla=Car(capacity='123CC',color='blue',name='Corolla')
+corolla.myMethod()
+privateCar1=PrivateCar(capacity='1244',ac='12',color='blue',name='Rahul')
+
+
+class Animal():
+    def __init__(self):
+        print("Animal Crwated")
+    def eat(self):
+        print("I a eatng")
+                
+class Dog(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+    def nakeSound(self):
+        return print("I bark")
+flix=Dog()
+flix.eat()
+flix.nakeSound()
